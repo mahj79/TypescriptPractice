5eae03ae821abe2096f07aded605bbf1f32d8e9d
Jack <101360909+mahj79@users.noreply.github.com>
2024-02-19 15:55:36 -0600

    "we are switching to an array"

---
+++ Elevator/Elevator.ts
@@ -0,0 +1,22 @@
+//main file for elevator project. requirements are up and down buttons (1st floor only up)
+//inside the elevator buttons to go to different floors
+
+const elevator = new Map<number, number>();
+for(let i = 0; i <= 100; i++){
+    elevator.set(i, i);
+}
+
+console.log(elevator);
+
+/*
+function elevatorSearch() {
+    for (let i = 0; i < elevator.length; i++) {
+        for (let j = i + 1; j < elevator.length + 1; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i + j]
+            }
+        }
+    }
+}
+
+*/
--- Elevator/ElevatorProject.ts
+++ Elevator/ElevatorProject.ts
@@ -1,15 +1,16 @@
 //main file for elevator project. requirements are up and down buttons (1st floor only up)
 //inside the elevator buttons to go to different floors
 
-const elevator = new Map<number, number>();
+const elevator: number[] = [];
 for(let i = 0; i <= 100; i++){
-    elevator.set(i, i);
+    elevator.push(i);
 }
 
 console.log(elevator);
 
-//confirm connection to git
 
+
+/*
 function elevatorSearch() {
     for (let i = 0; i < elevator.length; i++) {
         for (let j = i + 1; j < elevator.length + 1; j++) {
@@ -18,4 +19,4 @@ function elevatorSearch() {
             }
         }
     }
-}
+*/
